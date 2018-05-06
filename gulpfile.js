f94ceb786df93e3006762e20a77c1fce643a5cbe
Anton Lantukh <lantukh@bk.ru>
2018-05-07 00:46:20 +0300

    Создание шапки

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const gulp = require('gulp');
+
+gulp.task('copy', function() {
+  return gulp.src('source/**/*.{html,js,css}')
+    .on('data', function(file) {
+      console.log({
+        base: file.base
+      });
+    })
+    .pipe(gulp.dest('build'));
+});
